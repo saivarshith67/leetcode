c0713761388865c12112ccae31588bf11db6430e
Potta Sai Varshith <144475980+saivarshith67@users.noreply.github.com>
2026-06-02 13:57:46 +0530

    Time: 3 ms (87.36%), Space: 21.7 MB (6.32%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = {i : [] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visit, cycle = set(), set()
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+
+            if crs in visit:
+                return True
+
+            cycle.add(crs)
+
+            for pre in preMap[crs]:
+                if not dfs(pre): return False
+
+            cycle.remove(crs)
+            visit.add(crs)
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c): return False
+
+        return True
